abcd2198f8bfd1fd608a1dfe75919e9ccf8c342e
Tea-and-Coffee <awo.j.xd@gmail.com>
2021-09-25 19:45:40 +0900

    Initial Commit

---
+++ FastlaneExample/FastlaneExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneExampleApp.swift
+//  FastlaneExample
+//
+//  Created by arai on 2021/09/25.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
